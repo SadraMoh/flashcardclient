7ab70986afa02ff281c24e69ab1a6980c6c2f297
shockylemi <shockylemi@gmail.com>
2021-12-14 17:24:55 +0330

    account pages

--- src/app/account/account.module.ts
+++ src/app/account/account.module.ts
@@ -2,11 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SignupComponent } from './signup/signup.component';
 import { LoginComponent } from './login/login.component';
+import { AccountRoutingModule } from './account-routing.module';
+import { IonicModule } from '@ionic/angular';
 
 @NgModule({
   declarations: [SignupComponent, LoginComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    IonicModule,
+    AccountRoutingModule,
   ]
 })
 export class AccountModule { }
